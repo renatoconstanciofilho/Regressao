283b7dfd3675a8046ae6ba5701968e6e26d4cd1a
Renato Constancio Filho <renatoconstanciofilho@gmail.com>
2018-11-06 15:21:36 -0200

    Importado arquivo de dados e adicionado conteudo a variavel

--- rmdemo.m
+++ rmdemo.m
@@ -1,5 +1,10 @@
 clc;
-clear;
+clear y Fs;
 %https://www.mathworks.com/help/matlab/ref/load.html
-D = load(data,'-mat');
+f = load('data.mat');
 %A primeira coluna é o tamanho da casa, a segunda coluna é o número de quartos, e a terceira coluna é o preço da casa.
+
+%https://www.mathworks.com/help/matlab/import_export/load-parts-of-variables-from-mat-files.html
+%https://www.mathworks.com/matlabcentral/answers/182662-conversion-of-a-matrix-into-multiple-column-vectors
+X = f.data(:,1:2);
+Y = f.data(:,3);
